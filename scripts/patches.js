030742093e6478fe60f9380d9bfa74e72252764b
Nordiii <niklas907@gmail.com>
2020-08-25 00:17:17 +0200

    fix ruler movement so it is compatible with Show Drag Distance

--- scripts/patches.js
+++ scripts/patches.js
@@ -1,5 +1,4 @@
-import { MODULE_SCOPE, TOP_KEY, BOTTOM_KEY } from "./const.js";
-import { getWallBounds } from "./utils.js";
+import {getWallBounds} from "./utils.js";
 
 export function Patch_Token_onUpdate() {
     const oldOnUpdate = Token.prototype._onUpdate;
@@ -31,7 +30,7 @@ export function Patch_Token_onUpdate() {
         // if the original _onUpdate didn't perform a sight layer update,
         // but elevation has changed, do the update now
         if (changed.has("elevation") && !alreadyUpdated) {
-            canvas.sight.updateToken(this, { defer: true });
+            canvas.sight.updateToken(this, {defer: true});
             canvas.addPendingOperation("SightLayer.update", canvas.sight.update, canvas.sight);
             canvas.addPendingOperation("LightingLayer.update", canvas.lighting.update, canvas.lighting);
             canvas.addPendingOperation(`SoundLayer.update`, canvas.sounds.update, canvas.sounds);
@@ -57,22 +56,31 @@ export function Patch_WallCollisions() {
             return oldGetDarkVisionSight.apply(this, arguments);
         };
     }
+
     const oldOnUpdate = Token.prototype._onUpdate;
     Token.prototype._onUpdate = function (data, options) {
         currentTokenElevation = null;
         oldOnUpdate.apply(this, arguments);
     };
 
-    const oldCanDrag = Token.prototype._canDrag;
-    Token.prototype._canDrag = function (user, event) {
-        currentTokenElevation = event.target.data.elevation;
-        return oldCanDrag.apply(this, arguments);
+    const onDragLeftDrop = Token.prototype._onDragLeftDrop;
+    Token.prototype._onDragLeftDrop = function (event) {
+        const clones = event.data.clones || [];
+        if (clones.length !== 1) return onDragLeftDrop.apply(this, arguments);
+        currentTokenElevation = clones[0].data.elevation;
+        return onDragLeftDrop.apply(this, arguments);
     };
 
     const oldGetShiftedPosition = Token.prototype._getShiftedPosition;
     Token.prototype._getShiftedPosition = function (dx, dy) {
         currentTokenElevation = this.data.elevation;
        return oldGetShiftedPosition.apply(this, arguments);
+    };
+
+    const oldMoveToken = Ruler.prototype.moveToken;
+    Ruler.prototype.moveToken = function () {
+        currentTokenElevation = this._getMovementToken().data.elevation;
+        return oldMoveToken.apply(this, arguments);
     };
 
     const oldGetWallCollisionsForRay = WallsLayer.getWallCollisionsForRay;
